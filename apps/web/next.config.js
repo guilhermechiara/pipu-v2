be68c5a42b7291425af048e277c51d15be6b0b9b
Guilherme Chiara <guilhermechiara@Guilhermes-MacBook-Pro-2.local>
2025-03-31 21:11:39 -0300

    adding tailwind and shadcn as configs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ['@repo/ui'],
-};
+const nextConfig = {
+  transpilePackages: ["@workspace/ui"],
+}
+
+export default nextConfig
---
+++ apps/web/postcss.config.mjs
@@ -0,0 +1,3 @@
+import config from "@pipu/ui/postcss.config";
+
+export default config;
